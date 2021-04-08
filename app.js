907835b816458ce1c11771ae48edb3872a326922
dtoomey156 <71398662+dtoomey156@users.noreply.github.com>
2021-04-08 13:40:08 -0700

    create index html from command line

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-
+const fs = require("fs")
+const generatePage = require("./src/page-template.js")
 
 
 const profileDataArgs = process.argv.slice(2, process.argv.length)
 //console.log(profileDataArgs)
+// const name = profileDataArgs[0]
+// const github = profileDataArgs[1]
+const [name, github] = profileDataArgs
+
+
+
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err
+
+    console.log("Portfolio complete! Check out index.html to see the output!")
+})
 
 
 
@@ -13,22 +26,19 @@ const profileDataArgs = process.argv.slice(2, process.argv.length)
 
 
 
+// const printProfileData = profileDataArr => {
 
+//     // This...
+//     for (let i=0; i <profileDataArr.length; i ++) {
+//         console.log(profileDataArr[i])
+//     }
 
+//     console.log('===================')
 
-const printProfileData = profileDataArr => {
+//     // Is the same as this ...
 
-    // This...
-    for (let i=0; i <profileDataArr.length; i ++) {
-        console.log(profileDataArr[i])
-    }
+//     profileDataArr.forEach((profileItem) => console.log(profileItem))
 
-    console.log('===================')
+// }
 
-    // Is the same as this ...
-
-    profileDataArr.forEach((profileItem) => console.log(profileItem))
-    
-}
-
-printProfileData(profileDataArgs)
+// printProfileData(profileDataArgs)
